f6ab59555f18f1455914bf633ddbae71c2e26926
Romario Gomes <romariogomes02@gmail.com>
2016-08-14 17:16:14 -0300

    criacao das classes

---
+++ src/entidade/Produto.java
@@ -0,0 +1,55 @@
+package entidade;
+
+public class Produto {
+
+	private String nome;
+	private String fabricante;
+	private Integer quantidade;
+	private Double preco; 
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getFabricante() {
+		return fabricante;
+	}
+	
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Produto() {
+		
+	}
+
+	public Produto(String nome, String fabricante, Integer quantidade, Double preco) {
+		super();
+		this.nome = nome;
+		this.fabricante = fabricante;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+		
+}
---
+++ src/entidade/ProdutoNaoPerecivel.java
@@ -0,0 +1,45 @@
+package entidade;
+
+public class ProdutoNaoPerecivel extends Produto {
+
+	private Double altura;
+	private Double largura;
+	private Double peso;
+	
+	public ProdutoNaoPerecivel() {
+
+	}
+
+	public ProdutoNaoPerecivel(Double altura, Double largura, Double peso) {
+		
+		this.altura = altura;
+		this.largura = largura;
+		this.peso = peso;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	
+	
+}
